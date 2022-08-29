879414703079ac09337ec309db313a41f171b664
Manish Chand <manishtochand@gmail.com>
2022-08-30 00:00:36 +0530

    Add files via upload

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
