998ba7867cdb8373b0a2661bc707d6a7094cbb3b
Manish Chand <manishtochand@gmail.com>
2022-10-04 22:41:25 +0530

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['manishcad.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['manishcad.herokuapp.com', '127.0.0.1','*']
 
 
 # Application definition
